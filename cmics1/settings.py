4c5a9b05a654a0146af77f88916b5001cf8f5051
Prince kumar <50265671+princeku07@users.noreply.github.com>
2021-03-21 13:59:09 +0530

    Update settings.py

--- cmics1/settings.py
+++ cmics1/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj_database_url
+# import dj_database_url
 from decouple import config
 
 from django.contrib.messages import constants as messages
