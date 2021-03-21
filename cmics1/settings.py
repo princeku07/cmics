34889e8fc7adb5ad25a2a5cbeb2fdb9d5c75ec99
Prince kumar <50265671+princeku07@users.noreply.github.com>
2021-03-21 14:16:36 +0530

    Update settings.py

--- cmics1/settings.py
+++ cmics1/settings.py
@@ -13,11 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-# import dj_database_url
-# from decouple import config
+import dj_database_url
+from decouple import config
 
 from django.contrib.messages import constants as messages
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
