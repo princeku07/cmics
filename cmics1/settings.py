4c0c82ff8709988754239fa240daa8e308e5a8b6
Prince kumar <50265671+princeku07@users.noreply.github.com>
2021-03-21 15:48:09 +0530

    Update settings.py

--- cmics1/settings.py
+++ cmics1/settings.py
@@ -53,6 +53,21 @@ DEFAULT_APPS = [
 
     
 ]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 THIRD_PARTY_APPS =[
 
 ]
